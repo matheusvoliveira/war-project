d8f6a7881ff2568dbddf2efcca05f64d16698c44
Matheus Soares <matheusbraganca321@hotmail.com>
2023-05-16 10:38:21 -0300

    update Player and Dealer

--- blackjack.py
+++ blackjack.py
@@ -37,6 +37,9 @@ class Deck:
 value_list = []
 class Player:
     def __init__(self):
+        print(15 * '-')
+        print("Player's Round")
+        print(15 * '-')
         deck = Deck()
         self.deck_sufffle = deck.suffle()
         self.cards = deck.all_cards
@@ -53,28 +56,35 @@ class Player:
         print(soma)
         if soma < 21:
             return True
+
+    def get_value_list_sum(self):
+        soma = sum(value_list)
+        return soma
     def request(self):
         on = True
         while on:
             if self.value_list_sum():
-                hit_stand = input('Choose: \nHit[1] \nStand[2]: ')
+                hit_stand = input('Choose: Hit[1] / Stand[2]: ')
                 if hit_stand == '1':
                     print(self.cards[0])
-                    self.cards.pop(0)
                     value_list.append(self.cards[0].value)
+                    self.cards.pop(0)
+
                     print(value_list)
+                else:
+                    break
             else:
                 on = False
         print('tchau')
 
 
-# player = Player()
-# player.request()
 
 value_list_dealer = []
 class Dealer:
     def __init__(self):
-        print("Dealer's round")
+        print(15 * '-')
+        print("Dealer's cards")
+        print(15 * '-')
         deck = Deck()
         self.deck_sufffle = deck.suffle()
         self.cards = deck.all_cards
@@ -86,9 +96,35 @@ class Dealer:
         self.cards.pop(0)
         self.cards.pop(1)
 
+    def value_list_dealer_sum(self):
+        self.soma = sum(value_list_dealer)
+        if self.soma <= 17:
+            return True
+
+    def compare_dealer_player(self):
+        Player().get_value_list_sum()
+
+    def requesty(self):
+        print(15 * '-')
+        print("Dealer's round")
+        print(15 * '-')
+        on = True
+        while on:
+            if self.value_list_dealer_sum() or self.compare_dealer_player():
+                print(self.cards[0])
+                value_list_dealer.append(self.cards[0].value)
+                self.cards.pop(0)
+                print(value_list_dealer)
+            else:
+                on = False
+        print('tchau')
 
 
-dealer = Dealer()
+if __name__ == '__main__':
+    dealer = Dealer()
+    player = Player()
+    player.request()
+    dealer.requesty()
 
 
 
