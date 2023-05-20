2389fe1605a7aa98cee734a5f9e38bd501162489
Matheus Soares <100249571+matheusvoliveira@users.noreply.github.com>
2023-05-19 22:35:25 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -6,6 +6,7 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
          'Queen':10, 'King':10, 'Ace':11}
 
 
+
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
